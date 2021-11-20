38a16943a2730a20f5a3db69a5b5e612fc843156
Paulo Jean Alves Da Silva <paulojeanalvesdasilva@Paulos-MacBook-Pro.local>
2021-11-20 20:13:21 -0300

    Este commit aplicado cria a paginas de navegacao

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ConteudoComponent } from './conteudo/conteudo.component';
+import { HomeComponent } from './conteudo/home/home.component';
+import { UsuariosComponent } from './conteudo/usuarios/usuarios.component';
+import { ProdutosComponent } from './conteudo/produtos/produtos.component';
+import { LoginComponent } from './conteudo/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConteudoComponent,
+    HomeComponent,
+    UsuariosComponent,
+    ProdutosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
